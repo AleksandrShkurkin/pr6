4ceb1614d69e7b55bd2ccbd0e798ac6a7b5693eb
DeathRun4365 <118109901+DeathRun4365@users.noreply.github.com>
2022-11-13 14:17:22 +0200

    Add files via upload

---
+++ лр 4.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+#include "math.h"
+
+using namespace std;
+
+int main()
+{
+    float l;
+    cin >> l;
+    //cout << static_cast <int> (l) / 100;
+    cout << (int) l / 100;
+}
+
